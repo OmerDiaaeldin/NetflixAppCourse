c3a4ae6fb33f01de1c04e026fef8b3137bef05c0
Mohamed <KDP only@10.121.8.187>
2023-10-31 22:25:01 +0100

    Plan Class Modified & Subscription class created(still in need of billing method).

--- src/main/java/aui/netflixapp/subscriptionPlan/Plan.java
+++ src/main/java/aui/netflixapp/subscriptionPlan/Plan.java
@@ -13,7 +13,52 @@ import aui.netflixapp.subscriptionPlan.Quality;
 
 public class Plan {
     private double cost;
-    int screen_num;
-    int downloaded_devices;
-    Quality quality;
+    private int screen_num;
+    private int downloaded_devices;
+    private Quality quality;
+
+    public Plan(double cost, int screen_num, int downloaded_devices, Quality quality) {
+        this.cost = cost;
+        this.screen_num = screen_num;
+        this.downloaded_devices = downloaded_devices;
+        this.quality = quality;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public int getScreen_num() {
+        return screen_num;
+    }
+
+    public void setScreen_num(int screen_num) {
+        this.screen_num = screen_num;
+    }
+
+    public int getDownloaded_devices() {
+        return downloaded_devices;
+    }
+
+    public void setDownloaded_devices(int downloaded_devices) {
+        this.downloaded_devices = downloaded_devices;
+    }
+
+    public Quality getQuality() {
+        return quality;
+    }
+
+    public void setQuality(Quality quality) {
+        this.quality = quality;
+    }
+
+    @Override
+    public String toString() {
+        return "Plan{" + "cost=" + cost + ", screen_num=" + screen_num + ", downloaded_devices=" + downloaded_devices + ", quality=" + quality + '}';
+    }
+    
 }
