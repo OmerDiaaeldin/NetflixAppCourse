04a305230385a1cb11a0bb820a19b0fa731a859e
OmerDiaaeldin <omardiaaeldin2592001@gmail.com>
2023-10-31 22:48:04 +0100

    languages enum

---
+++ src/main/java/aui/netflixapp/shows/Languages.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aui.netflixapp.shows;
+
+/**
+ *
+ * @author odaio
+ */
+public enum Languages {
+    English, 
+    Spanish, 
+    French, 
+    German, 
+    Portuguese, 
+    Italian, 
+    Dutch, 
+    Swedish, 
+    Norwegian, 
+    Danish, 
+    Finnish, 
+    Russian, 
+    Arabic, 
+    Japanese, 
+    Korean, 
+    Chinese, 
+    Hindi, 
+    Turkish, 
+    Polish, 
+    Greek, 
+    Hungarian, 
+    Romanian, 
+    Czech, 
+    Thai, 
+    Hebrew, 
+    Indonesian, 
+    Malay, 
+    Vietnamese, 
+    Ukrainian;
+}
