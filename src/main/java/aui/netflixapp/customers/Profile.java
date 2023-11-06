7b45dcb2d8f783a4f08d2e9cff06c34eda3f88bb
Mohamed <KDP only@10.121.8.187>
2023-11-06 20:47:43 +0100

    User and Profile classes created, fixed array of strings instead of one string variable in some objects, added methods in user class.

--- src/main/java/aui/netflixapp/customers/Profile.java
+++ src/main/java/aui/netflixapp/customers/Profile.java
@@ -4,10 +4,121 @@
  */
 package aui.netflixapp.customers;
 
+
+import aui.netflixapp.shows.MaturityLevel;
+import aui.netflixapp.shows.Languages;
+import aui.netflixapp.shows.Show;
 /**
  *
  * @author odaio
  */
 public class Profile {
+    private String name;
+    private MaturityLevel level_restriction;
+    private String email;
+    private boolean toNotify;
+    private Languages language;
+    private boolean subtitles;
+    private Show[] favorites;
+    private Show[] blocked;
+    private Languages subtitle_lang;
+
+    public Profile(String name, MaturityLevel level_restriction, String email, boolean toNotify, Languages language, boolean subtitles, Show[] favorites, Show[] blocked, Languages subtitle_lang) {
+        this.name = name;
+        this.level_restriction = level_restriction;
+        this.email = email;
+        this.toNotify = toNotify;
+        this.language = language;
+        this.subtitles = subtitles;
+        this.favorites = favorites;
+        this.blocked = blocked;
+        this.subtitle_lang = subtitle_lang;
+    }
+    
+    public void modify_maturity() {
+        
+    }
+    
+    public void add_favorite() {
+        
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public MaturityLevel getLevel_restriction() {
+        return level_restriction;
+    }
+
+    public void setLevel_restriction(MaturityLevel level_restriction) {
+        this.level_restriction = level_restriction;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isToNotify() {
+        return toNotify;
+    }
+
+    public void setToNotify(boolean toNotify) {
+        this.toNotify = toNotify;
+    }
+
+    public Languages getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(Languages language) {
+        this.language = language;
+    }
+
+    public boolean isSubtitles() {
+        return subtitles;
+    }
+
+    public void setSubtitles(boolean subtitles) {
+        this.subtitles = subtitles;
+    }
+
+    public Show[] getFavorites() {
+        return favorites;
+    }
+
+    public void setFavorites(Show[] favorites) {
+        this.favorites = favorites;
+    }
+
+    public Show[] getBlocked() {
+        return blocked;
+    }
+
+    public void setBlocked(Show[] blocked) {
+        this.blocked = blocked;
+    }
+
+    public Languages getSubtitle_lang() {
+        return subtitle_lang;
+    }
+
+    public void setSubtitle_lang(Languages subtitle_lang) {
+        this.subtitle_lang = subtitle_lang;
+    }
+
+    @Override
+    public String toString() {
+        return "Profile{" + "name=" + name + ", level_restriction=" + level_restriction + ", email=" + email + ", toNotify=" + toNotify + ", language=" + language + ", subtitles=" + subtitles + ", favorites=" + favorites + ", blocked=" + blocked + ", subtitle_lang=" + subtitle_lang + '}';
+    }
+    
     
 }
--- src/main/java/aui/netflixapp/customers/User.java
+++ src/main/java/aui/netflixapp/customers/User.java
@@ -4,10 +4,81 @@
  */
 package aui.netflixapp.customers;
 
+
+import aui.netflixapp.payment.PaymentMethod;
+import aui.netflixapp.subscriptionPlan.Subscription;
+import aui.netflixapp.requests.Request;
 /**
  *
  * @author odaio
  */
 public class User {
+    private String phone;
+    private Profile[] profile;
+    private Request[] requests;
+    private PaymentMethod pay_method;
+    private Subscription subcription;
+
+    public User(String phone, Profile[] profile, Request[] requests, PaymentMethod pay_method, Subscription subcription) {
+        this.phone = phone;
+        this.profile = profile;
+        this.requests = requests;
+        this.pay_method = pay_method;
+        this.subcription = subcription;
+    }
+    
+    public void view_prof_history() {
+        
+    }
+    
+    public void download_hist() {
+        
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Profile[] getProfile() {
+        return profile;
+    }
+
+    public void setProfile(Profile[] profile) {
+        this.profile = profile;
+    }
+
+    public Request[] getRequests() {
+        return requests;
+    }
+
+    public void setRequests(Request[] requests) {
+        this.requests = requests;
+    }
+
+    public PaymentMethod getPay_method() {
+        return pay_method;
+    }
+
+    public void setPay_method(PaymentMethod pay_method) {
+        this.pay_method = pay_method;
+    }
+
+    public Subscription getSubcription() {
+        return subcription;
+    }
+
+    public void setSubcription(Subscription subcription) {
+        this.subcription = subcription;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "phone=" + phone + ", profile=" + profile + ", requests=" + requests + ", pay_method=" + pay_method + ", subcription=" + subcription + '}';
+    }
+    
     
 }
