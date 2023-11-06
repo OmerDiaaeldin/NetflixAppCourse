889ac237e8fd71d6abb2c286f4f6edd18f3d6992
Mohamed <KDP only@10.121.8.187>
2023-11-06 19:50:15 +0100

    Netflix class created.

--- src/main/java/aui/netflixapp/netflix/Netflix.java
+++ src/main/java/aui/netflixapp/netflix/Netflix.java
@@ -4,15 +4,61 @@
  */
 package aui.netflixapp.netflix;
 
+
+import aui.netflixapp.shows.Show;
+import aui.netflixapp.customers.Account;
+import aui.netflixapp.requests.Request;
 /**
  *
  * @author odaio
  */
 public class Netflix {
-    //list of shows
+    private Show[] shows;
+    private Account[] customers;
+    private Request[] requests;
+
+    public Netflix(Show[] shows, Account[] customers, Request[] requests) {
+        this.shows = shows;
+        this.customers = customers;
+        this.requests = requests;
+    }    
     
-    //list of accounts
-    //list of requests
+    public void register() {
+        
+    }
+    public void search() {
+        
+    }
+    public void browse() {
+        
+    }
+    public void addShow() {
+        
+    }
+
+    public Show[] getShows() {
+        return shows;
+    }
+
+    public void setShows(Show[] shows) {
+        this.shows = shows;
+    }
+
+    public Account[] getCustomers() {
+        return customers;
+    }
+
+    public void setCustomers(Account[] customers) {
+        this.customers = customers;
+    }
+
+    public Request[] getRequests() {
+        return requests;
+    }
+
+    public void setRequests(Request[] requests) {
+        this.requests = requests;
+    }
     
     
 }
